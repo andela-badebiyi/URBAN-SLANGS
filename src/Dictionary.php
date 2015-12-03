33f3e621cf01c62070a9f6be91aa39ee729ba28a
andela-badebiyi <bodunde.adebiyi@andela.com>
2015-12-03 13:37:43 +0100

    correction of bug

--- src/Dictionary.php
+++ src/Dictionary.php
@@ -1,8 +1,4 @@
 <?php
-	/*
-	*This class contains the a static array where all the urban
-	*slangs and their definitions would be temporarily stored
-	*/
 	class Dictionary{
 		public static $data = array();
 	}
